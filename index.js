224c8fc4e4c3ecb079e672e0531cb3324c9fa834
HanningtoneM <hanningtonepompyy@gmail.com>
2023-07-13 16:01:05 +0300

    Add more Math formulas

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ let x = 3.14
 x =Math.round(x); //round both up and down depending on the value
 x = Math.floor(x); // round down
 x = Math.ceil(x); // round up
+x = Math.pow(x, 2); // asssign a power 
+x = Math.sqrt(x); // squareroot
+x = Math.abs(x); //  to find absolute value
