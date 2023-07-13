fa9f32f9a967b22c06f8fbfb8936791a4c07d61b
HanningtoneM <hanningtonepompyy@gmail.com>
2023-07-13 16:05:15 +0300

    Add math.max

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 let x = 3.14
+let y = 5;
+let z = 9;
+let maximum;
+let minimum;
 
 x =Math.round(x); //round both up and down depending on the value
 x = Math.floor(x); // round down
@@ -6,3 +10,4 @@ x = Math.ceil(x); // round up
 x = Math.pow(x, 2); // asssign a power 
 x = Math.sqrt(x); // squareroot
 x = Math.abs(x); //  to find absolute value(distance away from zero)
+maximum = Math.max(x, y, z); // find maxium value
