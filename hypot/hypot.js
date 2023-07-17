8225a76af4ed1afe1ba8c0fdff3c65e35ad4de48
HanningtoneM <hanningtonepompyy@gmail.com>
2023-07-17 16:00:00 +0300

    Add functionality to calclulate the hypotenuse

--- hypot/hypot.js
+++ hypot/hypot.js
@@ -1,1 +1,18 @@
+let a;
+let b;
+let c;
+
+const btn = document.getElementById("submitButton");
+
+btn.addEventListener("click", function () {
+	a = document.getElementById("aTextBox").value;
+	a = Number(a);
+
+	b = document.getElementById("bTextBox").value;
+	b = Number(b);
+
+	c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+	document.getElementById("labelc").innerHTML = "Side C: " + c;
+});
 
