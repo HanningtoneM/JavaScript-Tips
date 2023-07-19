bf946df0a074f645b1d915f2f540fcdc81fd9f2c
HanningtoneM <hanningtonepompyy@gmail.com>
2023-07-19 15:41:09 +0300

    Add button to Javascript

---
+++ counter/counter.js
@@ -0,0 +1,16 @@
+const decreaseBtn = document.getElementById("decrease-button")
+const resetBtn = document.getElementById("reset-button")
+const increaseBtn = document.getElementById("increase-button")
+
+
+decreaseBtn.addEventListener("click", function(){
+    console.log("clicked")
+})
+
+resetBtn.addEventListener("click", function(){
+
+})
+
+increaseBtn.addEventListener("click", function(){
+    
+})
