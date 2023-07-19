2ccec1865b191160bf50b0ab2de4713f960a6e35
HanningtoneM <hanningtonepompyy@gmail.com>
2023-07-19 16:47:54 +0300

    Add new random.js file

---
+++ random/random.js
@@ -0,0 +1,7 @@
+let x = Math.floor(Math.random() * 6) + 1;
+let y = Math.floor(Math.random() * 6) + 1;
+let z = Math.floor(Math.random() * 6) + 1;
+
+console.log(x);
+console.log(y);
+console.log(z);
