c62217c5c468f90ff5bb260aa3207525f65f9341
HanningtoneM <hanningtonepompyy@gmail.com>
2023-07-19 15:54:18 +0300

    Format with prettier

--- counter/counter.js
+++ counter/counter.js
@@ -1,20 +1,20 @@
 let count = 0;
-const decreaseBtn = document.getElementById("decrease-button")
-const resetBtn = document.getElementById("reset-button")
-const increaseBtn = document.getElementById("increase-button")
-let countLabel = document.getElementById("countLabel")
+const decreaseBtn = document.getElementById("decrease-button");
+const resetBtn = document.getElementById("reset-button");
+const increaseBtn = document.getElementById("increase-button");
+let countLabel = document.getElementById("countLabel");
 
-decreaseBtn.addEventListener("click", function(){
-    count-=1;
-    countLabel.innerHTML = count
-})
+decreaseBtn.addEventListener("click", function () {
+	count -= 1;
+	countLabel.innerHTML = count;
+});
 
-resetBtn.addEventListener("click", function(){
-     count = 0;
-     countLabel.innerHTML = count
-})
+resetBtn.addEventListener("click", function () {
+	count = 0;
+	countLabel.innerHTML = count;
+});
 
-increaseBtn.addEventListener("click", function(){
-     count+=1;
-     countLabel.innerHTML =count
-})
+increaseBtn.addEventListener("click", function () {
+	count += 1;
+	countLabel.innerHTML = count;
+});
