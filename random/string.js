e2cce932b72864b2fb8b9c72714e8638031c039e
HanningtoneM <hanningtonepompyy@gmail.com>
2023-07-20 16:25:01 +0300

    Add trimand touppercase function

--- random/string.js
+++ random/string.js
@@ -1,4 +1,9 @@
-let userName = "Hanningtone Monda"
+let userName = "Hanningtone Monda  "
 console.log(userName.length)
 console.log(userName.charAt(0))
 console.log(userName.indexOf("i"))
+console.log(userName.lastIndexOf("e"))
+
+userName = userName.trim()
+userName = userName.toLocaleUpperCase()
+console.log(userName)
