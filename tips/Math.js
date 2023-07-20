dac817d88879a2f65affb7d4adf1c461ffb6e24a
HanningtoneM <hanningtonepompyy@gmail.com>
2023-07-20 15:02:11 +0300

    Add file to tips folder

---
+++ tips/Math.js
@@ -0,0 +1,15 @@
+let x = 3.14
+let y = 5;
+let z = 9;
+let maximum;
+let minimum;
+
+x =Math.round(x); //round both up and down depending on the value
+x = Math.floor(x); // round down
+x = Math.ceil(x); // round up
+x = Math.pow(x, 2); // asssign a power 
+x = Math.sqrt(x); // squareroot
+x = Math.abs(x); //  to find absolute value(distance away from zero)
+maximum = Math.max(x, y, z); // find maxium value
+minimum = Math.min(x, y, z); // find minimum
+x = Math.PI // display Pie
