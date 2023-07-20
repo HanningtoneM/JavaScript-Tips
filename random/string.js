26b7812c493b4e03bbf57e2630431e11ca7b57a2
HanningtoneM <hanningtonepompyy@gmail.com>
2023-07-20 16:28:53 +0300

    Add replaceAll function

--- random/string.js
+++ random/string.js
@@ -1,4 +1,5 @@
 let userName = "Hanningtone Monda  "
+let phoneNumber = "123-456-7890"
 console.log(userName.length)
 console.log(userName.charAt(0))
 console.log(userName.indexOf("i"))
@@ -6,4 +7,9 @@ console.log(userName.lastIndexOf("e"))
 
 userName = userName.trim()
 userName = userName.toLocaleUpperCase()
+userName = userName.toLocaleLowerCase()
+
+phoneNumber = phoneNumber.replaceAll("-", "9")
+
 console.log(userName)
+console.log(phoneNumber)
