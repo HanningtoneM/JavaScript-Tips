5b4aa4b5abb1e7f6291259e90a00dd64d180a7e1
HanningtoneM <hanningtonepompyy@gmail.com>
2023-07-25 15:46:55 +0300

    format code

--- random/string.js
+++ random/string.js
@@ -1,47 +1,44 @@
-let userName = "Hanningtone Monda  "
-let phoneNumber = "123-456-7890"
-console.log(userName.length) // length of string
-console.log(userName.charAt(0)) // where string is located
-console.log(userName.indexOf("i")) // number osf string
-console.log(userName.lastIndexOf("e")) // last index
+let userName = "Hanningtone Monda  ";
+let phoneNumber = "123-456-7890";
+console.log(userName.length); // length of string
+console.log(userName.charAt(0)); // where string is located
+console.log(userName.indexOf("i")); // number osf string
+console.log(userName.lastIndexOf("e")); // last index
 
-userName = userName.trim() // trim empty spaces
-userName = userName.toLocaleUpperCase() //uppercase
-userName = userName.toLocaleLowerCase() // lowercase
+userName = userName.trim(); // trim empty spaces
+userName = userName.toLocaleUpperCase(); //uppercase
+userName = userName.toLocaleLowerCase(); // lowercase
 
-phoneNumber = phoneNumber.replaceAll("-", "/") // replace the string
+phoneNumber = phoneNumber.replaceAll("-", "/"); // replace the string
 
-console.log(userName)
-console.log(phoneNumber)
-
+console.log(userName);
+console.log(phoneNumber);
 
 // slice method
 // extracts a section of a string to create a new string without
 // modyfing the existing string
 
-let fullName = "Hanningtone Monda"
+let fullName = "Hanningtone Monda";
 let firstName;
 let lastName;
 
-firstName = fullName.slice(0, fullName.indexOf(" "))
-lastName = fullName.slice(fullName.indexOf(" ")+1)
-console.log(lastName)
-console.log(firstName)
-
+firstName = fullName.slice(0, fullName.indexOf(" "));
+lastName = fullName.slice(fullName.indexOf(" ") + 1);
+console.log(lastName);
+console.log(firstName);
 
 // Method chaining = calling one method after another in one continous line of code
-let nameOne = "Monda"
-let letter = userName.charAt(0).toUpperCase().trim()
-console.log(letter)
-
+let nameOne = "Monda";
+let letter = userName.charAt(0).toUpperCase().trim();
+console.log(letter);
 
 // if statement = a basic form of decision making if a condition is true
 //                then do something if not , then donot do something
 
-let age = 21
+let age = 21;
 
-if(age >= 21) {
-    console.log("adult")
-} else if(age < 21) {
-    console.log("child")
+if (age >= 21) {
+	console.log("adult");
+} else if (age < 21) {
+	console.log("child");
 }
