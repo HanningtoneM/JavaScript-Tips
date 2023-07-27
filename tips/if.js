2fe2672ce6720d79e270f9530cad8c067d59ebf8
HanningtoneM <hanningtonepompyy@gmail.com>
2023-07-27 17:39:18 +0300

    Add radio button to js

--- tips/if.js
+++ tips/if.js
@@ -1,5 +1,8 @@
 const button = document.getElementById("button")
 const checkBox = document.getElementById("myCheckBox")
+const visaButton = document.getElementById("visaBtn")
+const masterBtn = document.get.getElementById("masterBtn")
+const paypalBtn = document.getElementById("paypalBtn")
 
 button.addEventListener("click", function(){
    if(checkBox.checked) {
