372034135f99cca8b280483459770b9ba8ba5947
HanningtoneM <hanningtonepompyy@gmail.com>
2023-07-27 17:44:55 +0300

    Add functionality to radio button

--- tips/if.js
+++ tips/if.js
@@ -1,14 +1,30 @@
 const button = document.getElementById("button")
 const checkBox = document.getElementById("myCheckBox")
 const visaButton = document.getElementById("visaBtn")
-const masterBtn = document.get.getElementById("masterBtn")
+const masterBtn = document.getElementById("masterBtn")
 const paypalBtn = document.getElementById("paypalBtn")
 
 button.addEventListener("click", function(){
-   if(checkBox.checked) {
+   if(checkBox.checked == true) {
     console.log("subscribed")
    }
    else {
     console.log("not subscribed")
+   }
+
+   if(visaButton.checked == true){
+    console.log("you are pating with Visa")
+   }
+
+   else if(masterBtn.checked == true) {
+    console.log("you are paying with master card")
+   }
+
+   else if(paypalBtn.checked == true) {
+    console.log("you are paying with paypal")
+   }
+
+   else {
+    console.log("select payment")
    }
 })
