172427a419f44a87b53f82b7e16ac88b9d13f4f1
HanningtoneM <hanningtonepompyy@gmail.com>
2023-07-31 16:56:54 +0300

    Ann or and AND operator

--- tips/if.js
+++ tips/if.js
@@ -68,3 +68,25 @@ switch(true){
     default:
         console.log(numberGrade, "Not a number")
 }
+
+// Gives us the ability to check more than 1 condition concurrently
+// && AND (Both conditions must be true)
+// || OR (either conditions must be true)
+
+
+let temp = 15;
+let sunny = true
+
+if(temp > 0 && temp < 30){
+    console.log("Weather is good")
+}
+else {
+    console.log("Bad weather")
+}
+
+if(temp <= 0 || temp >= 30) {
+    console.log("good weather")
+}
+else{
+    console.log("Weather is Bad")
+}
