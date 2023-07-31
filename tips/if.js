3579e1abe4fde15fe9cb407c53773e96a76628ca
HanningtoneM <hanningtonepompyy@gmail.com>
2023-07-31 16:34:52 +0300

    Add breaks to grades

--- tips/if.js
+++ tips/if.js
@@ -1,49 +1,47 @@
-const button = document.getElementById("button")
-const checkBox = document.getElementById("myCheckBox")
-const visaButton = document.getElementById("visaBtn")
-const masterBtn = document.getElementById("masterBtn")
-const paypalBtn = document.getElementById("paypalBtn")
+const button = document.getElementById("button");
+const checkBox = document.getElementById("myCheckBox");
+const visaButton = document.getElementById("visaBtn");
+const masterBtn = document.getElementById("masterBtn");
+const paypalBtn = document.getElementById("paypalBtn");
 
-button.addEventListener("click", function(){
-   if(checkBox.checked == true) {
-    console.log("subscribed")
-   }
-   else {
-    console.log("not subscribed")
-   }
+button.addEventListener("click", function () {
+	if (checkBox.checked == true) {
+		console.log("subscribed");
+	} else {
+		console.log("not subscribed");
+	}
 
-   if(visaButton.checked == true){
-    console.log("you are pating with Visa")
-   }
-
-   else if(masterBtn.checked == true) {
-    console.log("you are paying with master card")
-   }
-
-   else if(paypalBtn.checked == true) {
-    console.log("you are paying with paypal")
-   }
-
-   else {
-    console.log("select payment")
-   }
-})
+	if (visaButton.checked == true) {
+		console.log("you are pating with Visa");
+	} else if (masterBtn.checked == true) {
+		console.log("you are paying with master card");
+	} else if (paypalBtn.checked == true) {
+		console.log("you are paying with paypal");
+	} else {
+		console.log("select payment");
+	}
+});
 
 // switch = statement that examines a value for a match against many case clauses. more efficent than using many "else if"
 
-let grade = "A"
+let grade = "C";
 
-switch(grade){
-    case "A":
-     console.log("You did great")
-    case "B":
-      console.log("You did good")
-    case "C":
-        console.log("You did okay")
-    case "D":
-        console.log("You passed...barely")
-    case "F":
-        console.log("Failed")
-    default:
-        console.log("Not a letter Grade")    
+switch (grade) {
+	case "A":
+		console.log("You did great");
+		break;
+	case "B":
+		console.log("You did good");
+		break;
+	case "C":
+		console.log("You did okay");
+		break;
+	case "D":
+		console.log("You passed...barely");
+		break;
+	case "F":
+		console.log("Failed");
+		break;
+	default:
+		console.log("Not a letter Grade");
 }
