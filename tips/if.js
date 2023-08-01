527dc4d3438d9c4f2dbaa53901405bdbfe52abb5
HanningtoneM <hanningtonepompyy@gmail.com>
2023-08-01 17:04:01 +0300

    Remove and from if js

--- tips/if.js
+++ tips/if.js
@@ -68,25 +68,3 @@ switch(true){
     default:
         console.log(numberGrade, "Not a number")
 }
-
-// Gives us the ability to check more than 1 condition concurrently
-// && AND (Both conditions must be true)
-// || OR (either conditions must be true)
-
-
-let temp = 15;
-let sunny = true
-
-if(temp > 0 && temp < 30 && sunny){
-    console.log("Weather is good")
-}
-else {
-    console.log("Bad weather")
-}
-
-if(temp <= 0 || temp >= 30 || sunny) {
-    console.log("good weather")
-}
-else{
-    console.log("Weather is Bad")
-}
