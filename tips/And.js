f2051416d4f720e61afdf05cf5e46b0f0fe12ca3
HanningtoneM <hanningtonepompyy@gmail.com>
2023-08-01 17:04:17 +0300

    Add new file for AND operator

---
+++ tips/And.js
@@ -0,0 +1,21 @@
+// Gives us the ability to check more than 1 condition concurrently
+// && AND (Both conditions must be true)
+// || OR (either conditions must be true)
+
+
+let temp = 15;
+let sunny = true
+
+if(temp > 0 && temp < 30 && sunny){
+    console.log("Weather is good")
+}
+else {
+    console.log("Bad weather")
+}
+
+if(temp <= 0 || temp >= 30 || sunny) {
+    console.log("good weather")
+}
+else{
+    console.log("Weather is Bad")
+}
