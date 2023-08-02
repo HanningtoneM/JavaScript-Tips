21fdf4fd477532174ac65746ad841dd7bcbf6802
HanningtoneM <hanningtonepompyy@gmail.com>
2023-08-02 16:10:41 +0300

    Add code for not

---
+++ tips/not.js
@@ -0,0 +1,20 @@
+// !NOT logical operator
+// typically used to reserve aconditon 
+// true -> false false -> true
+
+let temp = 15;
+let sunny = true
+
+if(!(temp > 0)){
+    console.log("its cold outside")
+}
+else {
+    console.log("its cold outside")
+}
+
+if(!(sunny)) {
+	console.log("cold outside")
+}
+else(
+	console.log("its warm outside")
+)
