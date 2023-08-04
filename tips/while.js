d1a81e139b592aa6ddd1f7f986d0dbeb8ad2e5db
HanningtoneM <hanningtonepompyy@gmail.com>
2023-08-04 15:34:22 +0300

    format code

--- tips/while.js
+++ tips/while.js
@@ -8,7 +8,7 @@ console.log("hello", userName);
 // do while loop = do something then check if the condition repeat if the condion is true
 
 let user;
-do{
-    user = window.prompt("Enteer your name?")
-}while(user === "")
-console.log("Hello,", user )
+do {
+	user = window.prompt("Enteer your name?");
+} while (user === "");
+console.log("Hello,", user);
