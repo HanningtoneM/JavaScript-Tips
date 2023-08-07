e5a963e7e8e698a37439868c436661da63213520
HanningtoneM <hanningtonepompyy@gmail.com>
2023-08-07 15:56:21 +0300

    Add break for loop

--- tips/for.js
+++ tips/for.js
@@ -2,3 +2,14 @@
 for (let i = 1; i <= 10; i += 2) {
 	console.log(i);
 }
+
+// break =breaks out of a loop entirely
+// continue = skips one iteration of a loop
+
+for(let i = 1; i<=20; i++){
+	if(i===13){
+		break
+	}
+	console.log(i)
+}
+
