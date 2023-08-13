6f63b639eefdd1d961960bbd05d2afc3c71568fc
HanningtoneM <hanningtonepompyy@gmail.com>
2023-08-13 13:25:26 +0300

    Add code to random

--- random/random.js
+++ random/random.js
@@ -1,18 +1,18 @@
-const rollButton = document.getElementById("random-button")
-let xLabel = document.getElementById("xLabel")
-let yLabel = document.getElementById("yLabel")
-let zLabel = document.getElementById("zLabel")
+const rollButton = document.getElementById("random-button");
+let xLabel = document.getElementById("xLabel");
+let yLabel = document.getElementById("yLabel");
+let zLabel = document.getElementById("zLabel");
 
 let x;
-let y; 
-let z; 
+let y;
+let z;
 
-rollButton.addEventListener("click", function(){
-    x = Math.floor(Math.random() * 6) + 1;
-    y = Math.floor(Math.random() * 6) + 1;
-    z = Math.floor(Math.random() * 6) + 1;
-    
-    xLabel.textContent = x
-    yLabel.textContent = y
-    zLabel.textContent = z
-})
+rollButton.addEventListener("click", function () {
+	x = Math.floor(Math.random() * 6) + 1;
+	y = Math.floor(Math.random() * 6) + 1;
+	z = Math.floor(Math.random() * 6) + 1;
+
+	xLabel.textContent = x;
+	yLabel.textContent = y;
+	zLabel.textContent = z;
+});
